f4639a9d1780816cd2e61a8f56f2d71116c099e9
botelja <ivo.surjak@gmail.com>
2019-11-04 01:05:22 +0100

    Cleanup, Navbar, Home, About component

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <p className="h4 mt-2">
+        <i className={icon} />
+        {title}
+      </p>
+
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'far fa-address-book mr-1'
+};
+
+export default Navbar;
