79306d9fc2f4399c443891db280b65e747a385a1
botelja <ivo.surjak@gmail.com>
2019-11-04 16:34:43 +0100

    Delete contact from ui

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -54,6 +54,12 @@ const ContactState = (props) => {
   };
 
   //Delete Contact
+  const deleteContact = (id) => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  };
 
   //Set Current Contact
 
@@ -66,7 +72,9 @@ const ContactState = (props) => {
   //Clear Filter Contacts
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
+    <ContactContext.Provider
+      value={{ contacts: state.contacts, addContact, deleteContact }}
+    >
       {props.children}
     </ContactContext.Provider>
   );
